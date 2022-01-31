a0ea64c51b7df29f1600394141d73aa18dfbff4a
Arif5H <arifhh@code.edu.az>
2022-01-31 18:58:32 +0400

    Task6

---
+++ script.js
@@ -0,0 +1,8 @@
+function button() {
+    let first = Math.floor(Math.random()*300)+10
+    
+
+    document.getElementById("square").style.width= first +"px";
+    document.getElementById("square").style.height= first +"px";
+    
+}
